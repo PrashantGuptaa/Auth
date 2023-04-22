df5d2036b34bd484221c1400b7f979a8b4e4065d
PrashantGuptaa <meengineerprashant@gmail.com>
2023-04-22 20:59:19 +0530

    Added routes

--- configs/routes.js
+++ configs/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const healthCheckController = require('../controllers/health.controller');
-const { registerUserController } = require('../controllers/user.controller');
+const { registerUserController, loginUserController } = require('../controllers/user.controller');
 
 const router =express.Router();
 
-router.get('/', healthCheckController);
+router.get('/health', healthCheckController);
 router.post('/register', registerUserController);
+router.post('/login', loginUserController);
+
 
 module.exports = router;
